2fe6f2304dcb8ec4f2ad31de7d92b9d32df8b94d
ilhamudn <79589091+ilhamudn@users.noreply.github.com>
2022-01-22 03:01:35 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost:8080/web_pweb/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
